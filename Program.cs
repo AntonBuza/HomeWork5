c08ed1abb1796590b0276ae01fdd8b2975d0d45d
AntonBuza <124317253+AntonBuza@users.noreply.github.com>
2023-03-03 12:51:24 +0700

    Add lesson 3

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+
+double[] GenArray(int size, int leftRange, int rightRange)
+{
+    double[] array = new double[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange)+rand.NextDouble(); 
+    }
+    
+    return array;
+}
+
+void PrintArray(double[] array)
+    {
+         Console.WriteLine("[" + string.Join(", ", array) + "]");
+    }
+
+double Diff (double[] array)
+{
+     double min = array[0];
+     double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {  min = array[i]; }
+        else if (array[i] > max)
+        {  max = array[i]; }
+    }
+    double diff = max - min;
+    return diff;
+}
+
+
+double[] array = GenArray(5, -10, 10);
+PrintArray(array);
+double diff = Diff(array);
+System.Console.WriteLine(diff);
+
